12980302bf6d75931c4456cad5d9813ed4798b13
Mészáros Mihály <misi@niif.hu>
2017-02-02 11:23:54 +0100

    comment not used "use"-s

---
+++ src/Actions/TokenAction.php
@@ -0,0 +1,58 @@
+<?php
+namespace OAuth2Server\Actions;
+
+
+use League\OAuth2\Server\AuthorizationServer;
+use League\OAuth2\Server\Exception\OAuthServerException;
+//use League\OAuth2\Server\Grant\AuthCodeGrant;
+use OAuth2Server\Entities\UserEntity;
+//use OAuth2Server\Repositories\AccessTokenRepository;
+//use OAuth2Server\Repositories\ClientRepository;
+//use OAuth2Server\Repositories\RefreshTokenRepository;
+//use OAuth2Server\Repositories\ScopeRepository;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+//use Slim\App;
+use Zend\Diactoros\Stream;
+
+use Psr\Log\LoggerInterface;
+
+final class TokenCodeAction
+{
+    private $logger;
+
+    private $authserver;
+
+    public function __construct(AuthorizationServer $authserver, LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+        $this->authserver = $authserver;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $args)
+    {
+        $this->logger->info("Home page action dispatched");
+        try {
+            // Validate the HTTP request and return an AuthorizationRequest object.
+            // The auth request object can be serialized into a user's session
+            $authRequest = $this->authserver->validateAuthorizationRequest($request);
+
+            // Once the user has logged in set the user on the AuthorizationRequest
+            $authRequest->setUser(new UserEntity());
+
+            // Once the user has approved or denied the client update the status
+            // (true = approved, false = denied)
+            $authRequest->setAuthorizationApproved(true);
+
+            // Return the HTTP redirect response
+            return $this->authserver->completeAuthorizationRequest($authRequest, $response);
+        } catch (OAuthServerException $exception) {
+            return $exception->generateHttpResponse($response);
+        } catch (\Exception $exception) {
+            $body = new Stream('php://temp', 'r+');
+            $body->write($exception->getMessage());
+
+            return $response->withStatus(500)->withBody($body);
+        }
+    }
+}
