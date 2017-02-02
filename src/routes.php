0bb92cdf1f20f6f584d870952177687d27dc9257
Mészáros Mihály <misi@niif.hu>
2017-02-02 21:21:51 +0100

    change get to post

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 // Routes
-$app->get('/auth', OAuth2Server\Actions\AuthCodeAction::class)
+$app->post('/auth', OAuth2Server\Actions\AuthCodeAction::class)
     ->setName('auth_code');
 
-$app->get('/token', OAuth2Server\Actions\TokenAction::class)
+$app->post('/token', OAuth2Server\Actions\TokenAction::class)
     ->setName('token');
