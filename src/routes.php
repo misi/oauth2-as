90b9ba9744e0bc46cdbe82682a0aadbc1ec5babc
Mészáros Mihály <misi@niif.hu>
2017-02-02 09:25:42 +0100

    htaccess

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,4 @@
 <?php
 // Routes
-$app->get('/auth_code', OAuth2Server\Action\AuthCodeAction::class)
+$app->get('as/auth_code', OAuth2Server\Action\AuthCodeAction::class)
     ->setName('auth_code');
