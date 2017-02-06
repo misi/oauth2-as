319bd1f2bdf38905544c8b298eb341848d9c454d
Mészáros Mihály <misi@niif.hu>
2017-02-06 19:00:00 +0100

    delete unwanted ':'

--- src/dependencies.php
+++ src/dependencies.php
@@ -14,7 +14,6 @@ use League\OAuth2\Server\Grant\ImplicitGrant;
 use League\OAuth2\Server\Grant\PasswordGrant;
 use League\OAuth2\Server\Grant\RefreshTokenGrant;
 use PDO;
-:
 
 // DIC configuration
 $container = $app->getContainer();
