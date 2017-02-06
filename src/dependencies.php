a24a9da83ea2120a977a879dbe5e2d4e4adeb64f
Mészáros Mihály <misi@niif.hu>
2017-02-06 16:37:01 +0100

    add pdo

--- src/dependencies.php
+++ src/dependencies.php
@@ -13,7 +13,7 @@ use League\OAuth2\Server\Grant\ClientCredentialsGrant;
 use League\OAuth2\Server\Grant\ImplicitGrant;
 use League\OAuth2\Server\Grant\PasswordGrant;
 use League\OAuth2\Server\Grant\RefreshTokenGrant;
-
+use PDO;
 
 
 // DIC configuration
