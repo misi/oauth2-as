8469a8f41a26a633a570f7635ba93e120fa938bc
Mészáros Mihály <misi@niif.hu>
2017-02-07 11:51:12 +0100

    unify

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -38,11 +38,13 @@ class UserRepository implements UserRepositoryInterface
         ClientEntityInterface $clientEntity
     ) {
         $this->logger->info("user: ".$username."\nclient_id: ". $clientEntity->getIdentifier());
-        $sql='SELECT * FROM `acl`
+
+        $sql="SELECT * FROM `acl`
                   LEFT JOIN `client` ON `client`.`id` = `acl`.`client_id`
                   LEFT JOIN `user` ON `user`.`id` = `acl`.`user_id`
                   WHERE `user`.`username` = :username
-                   AND  `client`.`id` = :client_id';
+                   AND  `client`.`id` = :client_id";
+
         $stmt=$this->pdo->prepare($sql);
         $stmt->bindParam(':username', $username, PDO::PARAM_STR);
         $stmt->bindParam(':client_id', $clientEntity->getIdentifier(), PDO::PARAM_STR);
