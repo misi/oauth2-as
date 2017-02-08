49005a789969871b72cbfe00a356dec0c6ef075d
Mészáros Mihály <misi@niif.hu>
2017-02-08 10:46:56 +0100

    fix mistake . vs ->

--- src/Repositories/ScopeRepository.php
+++ src/Repositories/ScopeRepository.php
@@ -113,7 +113,7 @@ class ScopeRepository implements ScopeRepositoryInterface
         }
       }
 
-      $this->logger.info("Valid_Scopes: ".print_r($valid_scopes,true));
+      $this->logger->info("Valid_Scopes: ".print_r($valid_scopes,true));
 
       return $valid_scopes;
     }
