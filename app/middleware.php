fd5c8711bb167b77fb5beb08a65fdfe06e846452
Mészáros Mihály <misi@niif.hu>
2017-02-14 13:56:07 +0100

    Add session

--- app/middleware.php
+++ app/middleware.php
@@ -2,3 +2,6 @@
 // Application middleware
 // e.g: $app->add(new \Slim\Csrf\Guard);
 $app->add(new \Slim\Csrf\Guard);
+
+// Session
+$app->add(new \Slim\Middleware\Session($app->get('settings')['session']));
