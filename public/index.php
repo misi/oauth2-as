6a14b50657c97f05386d720c27e61d36fb010c59
Mészáros Mihály <misi@niif.hu>
2017-02-14 12:53:59 +0100

    Remove session_start

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-session_start();
 // Instantiate the app
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
