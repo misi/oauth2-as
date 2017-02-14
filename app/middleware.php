da0aabcedabbfbbed6aa68c6b7c0f3f4dc9e61bf
Mészáros Mihály <misi@niif.hu>
2017-02-14 16:11:50 +0100

    session first

--- app/middleware.php
+++ app/middleware.php
@@ -1,7 +1,7 @@
 <?php
 // Application middleware
-// e.g: $app->add(new \Slim\Csrf\Guard);
-$app->add(new \Slim\Csrf\Guard);
-
 // Session
 $app->add(new \Slim\Middleware\Session($container->get('settings')['session']));
+
+// e.g: $app->add(new \Slim\Csrf\Guard);
+$app->add(new \Slim\Csrf\Guard);
