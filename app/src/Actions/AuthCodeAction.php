56fbb335d75601f5482a86ed37050401bc6c3fc5
Mészáros Mihály <misi@niif.hu>
2017-02-15 09:16:13 +0100

    add some spaces

--- app/src/Actions/AuthCodeAction.php
+++ app/src/Actions/AuthCodeAction.php
@@ -84,12 +84,14 @@ final class AuthCodeAction
 
                   // Once the user has approved or denied the client update the status
                   // (true = approved, false = denied)
-                  $authRequest->setAuthorizationApproved(true);                
+                  $authRequest->setAuthorizationApproved(true);
+
               } else {
 
                   // Once the user has approved or denied the client update the status
                   // (true = approved, false = denied)
                   $authRequest->setAuthorizationApproved(false);
+                  
               }
 
               // remove redis
