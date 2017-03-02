2451861e8452a9da1dc1514a4b1f99daaeb83f41
Mészáros Mihály <misi@niif.hu>
2017-03-02 11:04:34 +0100

    moved to config

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,14 @@
 <?php
 // Routes
 
+// authserver settings
+$settings = $container->get('settings')['authserver'];
+
 //GET
-$app->get('/authorize', OAuth2Server\Actions\AuthCodeAction::class);
+$app->get($settings['authorize_url'], OAuth2Server\Actions\AuthCodeAction::class);
 
 //POST
-$app->post('/authorize', OAuth2Server\Actions\AuthCodeAction::class);
+$app->post($settings['authorize_url'], OAuth2Server\Actions\AuthCodeAction::class);
 
-$app->post('/access_token', OAuth2Server\Actions\TokenAction::class)
+$app->post($settings['access_token_url'], OAuth2Server\Actions\TokenAction::class)
     ->setName('token');
